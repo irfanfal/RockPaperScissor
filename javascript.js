9f7992a83dea59cd22e8272fb1fe38fdf3caf740
irfanfal <falihmahdika@gmail.com>
2023-08-06 19:13:34 +0700

    modifikasi code di javascript.js

--- javascript.js
+++ javascript.js
@@ -11,60 +11,78 @@ function playRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection){
         return "draw";
     }else if(playerSelection === "batu" && computerSelection === "kertas"){
+        com_score++;
         return "lose";
     }else if(playerSelection === "batu" && computerSelection === "gunting"){
+        player_score++;
         return "win";
     }else if(playerSelection === "kertas" && computerSelection === "gunting"){
+        com_score++;
         return "lose";
     }else if(playerSelection === "kertas" && computerSelection === "batu"){
+        player_score++;
         return "win";
     }else if(playerSelection === "gunting" && computerSelection === "batu"){
+        com_score++;
         return "lose";
     }else if(playerSelection === "gunting" && computerSelection === "kertas"){
+        player_score++;
         return "win";
     }
 }
 
 function game(playerChoice){
     let comChoice = com_Random();
-
     condition = playRound(playerChoice, comChoice);
-    let count = 0;
-    count += 1;
     console.log(condition);
-    console.log(count);
-}
+    games_play ++;
+    if(games_play < 5){
+        game_start();
+        console.log(com_score);
+        console.log(player_score);
 
-const batuImg = document.getElementById("batu");
-const kertasImg = document.getElementById("kertas");
-const guntingImg = document.getElementById("gunting");
+    }else{
+        batuImg.removeEventListener("click", clickBatu);
+        kertasImg.removeEventListener("click", clickKertas);
+        guntingImg.removeEventListener("click", clickGunting);    
+        console.log("Game OVer");
+    }
+
+
+}
 
 function clickBatu(){
     playerSelect = "batu";
     game(playerSelect);
-    batuImg.removeEventListener("click", clickBatu);
-    kertasImg.removeEventListener("click", clickKertas);
-    guntingImg.removeEventListener("click", clickGunting);
 }
 
 function clickKertas(){
     playerSelect = "kertas";
     game(playerSelect);
-    batuImg.removeEventListener("click", clickBatu);
-    kertasImg.removeEventListener("click", clickKertas);
-    guntingImg.removeEventListener("click", clickGunting);
 }
 
 function clickGunting(){
     playerSelect = "gunting";
     game(playerSelect);
-    batuImg.removeEventListener("click", clickBatu);
-    kertasImg.removeEventListener("click", clickKertas);
-    guntingImg.removeEventListener("click", clickGunting);
 }
 
 
-batuImg.addEventListener("click", clickBatu);
-kertasImg.addEventListener("click", clickKertas);
-guntingImg.addEventListener("click", clickGunting);
+function game_start(){
+    batuImg.addEventListener("click", clickBatu);
+    kertasImg.addEventListener("click", clickKertas);
+    guntingImg.addEventListener("click", clickGunting);
+}
 
+let player_score = 0;
+let com_score = 0;
+let games_play = 0;
+
+
+const batuImg = document.getElementById("batu");
+const kertasImg = document.getElementById("kertas");
+const guntingImg = document.getElementById("gunting");
+
+
+
+
+game_start();
